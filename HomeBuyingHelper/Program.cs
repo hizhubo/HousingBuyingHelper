9ecfa8fee1aa28e6061ef267ad26b3a7238a80a0
Bo Zhu <Bo Zhu>
2017-09-12 21:57:06 -0700

    Add project files.

---
+++ HomeBuyingHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeBuyingHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
